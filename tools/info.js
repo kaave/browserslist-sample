a922fbb98badfff2180a3d8705a8fc1ed6836dee
kaave <abe@framelunch.jp>
2017-06-04 23:12:37 +0900

    Add autoprefixer info

--- tools/info.js
+++ tools/info.js
@@ -1,4 +1,5 @@
 const browserslist = require('browserslist');
+const autoprefixer = require('autoprefixer');
 
 const packageJson = require('../package.json');
 
@@ -9,5 +10,7 @@ console.log(packageJson.browserslist);
 console.log('\n---Target browsers---');
 console.log(browsers);
 console.log('\n---Coverage---');
-console.log(browserslist.coverage(browsers));
+console.log(`${browserslist.coverage(browsers)}%`);
+console.log('\n---autoprefixer info---');
+console.log(autoprefixer({ browsers: packageJson.browserslist }).info());
 
