6f77e05c8dcc7891dea5ee02e671a9bdc8d9cc8c
kaave <abe@framelunch.jp>
2017-06-15 21:47:52 +0900

    Fix gulp-autoprefixer

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -3,8 +3,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 
 gulp.task('default', () => (
-  gulp.src('source/index.css')
+  gulp.src('../source/index.css')
     .pipe(autoprefixer())
     .pipe(rename('gulp-autoprefixer.css'))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('../build/'))
 ));
